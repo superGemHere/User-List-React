e9eae6e3b4178cd7fb39fc9b362328eef942761a
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-03 20:28:24 +0200

    import css

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './styles.css'
+function App() {
+
+  return (
+    <>
+    <h1>Hello React</h1>    
+    </>
+  )
+}
+
+export default App
