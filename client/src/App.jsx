44f3b96bd9a7cddc891ce00ab5a036555256fc9b
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-03 21:18:14 +0200

    +search

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
 
 import Footer from './components/Footer'
 import Header from './components/Header'
+import UserList from "./components/UserList";
+
 import './styles.css'
 function App() {
 
   return (
     <>
     <Header />  
+    <main className="main">
+      <UserList />
 
+    </main>
 
     <Footer />
     </>
---
+++ client/src/components/UserList.jsx
@@ -0,0 +1,9 @@
+import Search from "./Search";
+
+export default function UserList() {
+    return (
+        <section className="card users-container">
+            <Search />
+        </section>
+    );
+}
