0cc5c1af549cb757d4cfe6d4428a20bf038a5499
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-03 20:58:46 +0200

    +footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import Footer from './components/Footer'
+import Header from './components/Header'
 import './styles.css'
 function App() {
 
   return (
     <>
-    <h1>Hello React</h1>    
+    <Header />  
+
+
+    <Footer />
     </>
   )
 }
