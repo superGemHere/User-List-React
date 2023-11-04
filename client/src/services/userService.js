071d67d4b20118c4080bb9dacbf575f1d262f53a
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-04 15:25:43 +0200

    +initiate userService
    User API calls

---
+++ client/src/services/userService.js
@@ -0,0 +1,16 @@
+const baseUrl = 'http://localhost:3030/jsonstore/users';
+
+export const getAll = async() =>{
+    try {
+        const response = await fetch(baseUrl);
+        const result = await response.json();
+
+        const data = Object.values(result)
+        console.log(data);
+        
+
+        return data;
+    } catch (error) {
+        alert(error)
+    }
+}
