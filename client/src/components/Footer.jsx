d13b49aac6f789906aee7dbc5a13f7c4609f41b6
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-04 15:27:50 +0200

    modify Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return (
         <footer className="footer">
-        <p>This site is designed to be used for training purposes at SoftUni.</p>
+        <p>This site is just a made for training purpose.</p>
       </footer>
     );
 }
