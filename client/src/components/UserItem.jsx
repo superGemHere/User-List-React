41ef6d522acc4bff3482365c40932cfd638e0f2f
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-06 17:35:54 +0200

    .modify info button

--- client/src/components/UserItem.jsx
+++ client/src/components/UserItem.jsx
@@ -1,13 +1,20 @@
 import { formatDate } from "../utils/dateUtils";
 
 export default function UserItem({
+    userId,
     firstName,
     lastName,
     email,
     phoneNumber,
     createdAt,
-    imageUrl
+    imageUrl,
+    onDetailsClick
+
 }){
+    const infoClickHandler = () => {
+      onDetailsClick(userId);
+    };
+
     return (
         <tr>
         <td>
@@ -38,7 +45,7 @@ export default function UserItem({
               </path>
             </svg>
           </button>
-          <button className="btn info-btn" title="Info">
+          <button className="btn info-btn" title="Info" onClick={infoClickHandler}>
             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="info"
               className="svg-inline--fa fa-info" role="img" xmlns="http://www.w3.org/2000/svg"
               viewBox="-150 0 512 612">
