787c9bedbdd241b71ad5ff69ccba0d28633314bc
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-07 19:38:58 +0200

    + import trough props event handlers

--- client/src/components/UserItem.jsx
+++ client/src/components/UserItem.jsx
@@ -8,12 +8,17 @@ export default function UserItem({
     phoneNumber,
     createdAt,
     imageUrl,
-    onDetailsClick
+    onDetailsClick,
+    onDeleteClick
 
 }){
     const infoClickHandler = () => {
       onDetailsClick(userId);
     };
+
+    const deleteClickHandler = () => {
+      onDeleteClick(userId);
+    }
 
     return (
         <tr>
@@ -37,7 +42,7 @@ export default function UserItem({
               </path>
             </svg>
           </button>
-          <button className="btn delete-btn" title="Delete">
+          <button className="btn delete-btn" title="Delete" onClick={deleteClickHandler}>
             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash"
               className="svg-inline--fa fa-trash" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 498 512">
               <path fill="currentColor"
